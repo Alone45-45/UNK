3ceb7acaf8753f8a00a49cb182e945ccf79e60e3
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-07-02 19:48:45 +0530

    Update cust_filters_sql.py

--- UNK/modules/sql/cust_filters_sql.py
+++ UNK/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from PglRobot.modules.helper_funcs.msg_types import Types
-from PglRobot.modules.sql import BASE, SESSION
+from UNK.modules.helper_funcs.msg_types import Types
+from UNK.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
