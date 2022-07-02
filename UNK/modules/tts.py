a4df44f25c0b81f93c074763cdc460a25db03a0d
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-07-02 19:09:48 +0530

    Update tts.py

--- UNK/modules/tts.py
+++ UNK/modules/tts.py
@@ -12,10 +12,10 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
-from PglRobot import dispatcher
-from PglRobot.__main__ import STATS
-from PglRobot.modules.disable import DisableAbleCommandHandler
-from PglRobot.modules.helper_funcs.extraction import extract_user
+from UNK import dispatcher
+from UNK.__main__ import STATS
+from UNK.modules.disable import DisableAbleCommandHandler
+from UNK.modules.helper_funcs.extraction import extract_user
 
 
 def tts(update, context):
