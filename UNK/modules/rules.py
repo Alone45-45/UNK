d5721a8595e4742d8fc07ac585f19a8b78015ad5
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-07-02 19:22:47 +0530

    Update rules.py

--- UNK/modules/rules.py
+++ UNK/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import PglRobot.modules.sql.rules_sql as sql
-from PglRobot import dispatcher
-from PglRobot.modules.helper_funcs.chat_status import user_admin
-from PglRobot.modules.helper_funcs.string_handling import markdown_parser
+import UNK.modules.sql.rules_sql as sql
+from UNK import dispatcher
+from UNK.modules.helper_funcs.chat_status import user_admin
+from UNK.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
