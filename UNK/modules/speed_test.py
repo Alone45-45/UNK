094a512b5a05146a300314ff598f357ea52ba694
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-07-02 19:11:02 +0530

    Update speed_test.py

--- UNK/modules/speed_test.py
+++ UNK/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from PglRobot import DEV_USERS, dispatcher
-from PglRobot.modules.disable import DisableAbleCommandHandler
-from PglRobot.modules.helper_funcs.chat_status import dev_plus
+from UNK import DEV_USERS, dispatcher
+from UNK.modules.disable import DisableAbleCommandHandler
+from UNK.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
