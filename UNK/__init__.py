286ddb7cd3122ac6fd0b4826b8facb93485a8109
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-07-10 01:57:35 +0530

    Update __init__.py

--- UNK/__init__.py
+++ UNK/__init__.py
@@ -6,17 +6,26 @@ import spamwatch
 
 import telegram.ext as tg
 from pyrogram import Client, errors
+from pymongo import MongoClient
 from telethon import TelegramClient
+from telethon.sessions import MemorySession
+from Python_ARQ import ARQ
+from aiohttp import ClientSession
+from pyrogram.types import Message
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
+from pyrogram.types import Chat, User
+from inspect import getfullargspec
 
 StartTime = time.time()
 
 # enable logging
+FORMAT = "[UNK] %(message)s"
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
+    format=FORMAT,
+    datefmt="[%X]",
 )
-
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
@@ -26,7 +35,17 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     )
     quit(1)
 
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
 ENV = bool(os.environ.get("ENV", False))
+aiohttpsession = ClientSession()
 
 if ENV:
     TOKEN = os.environ.get("TOKEN", None)
@@ -64,12 +83,27 @@ if ENV:
     EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
     URL = os.environ.get("URL", "")  # Does not contain token
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
+    BOT_NAME = os.environ.get("BOT_NAME", "")
+    START_IMG = os.environ.get("START_IMG", "")
+    ASS_USERNAME = os.environ.get("ASS_USERNAME", "")
+    BOT_TUT = os.environ.get("BOT_TUT", "")
+    MUSICBOT_TUT = os.environ.get("MUSICBOT_TUT", "")
     PORT = int(os.environ.get("PORT", 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     DB_URI = os.environ.get("DATABASE_URL")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
+    BOT_ID = int(os.environ.get("BOT_ID", None))
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
@@ -80,14 +114,16 @@ if ENV:
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
-    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
     WALL_API = os.environ.get("WALL_API", None)
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "")
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    ARQ_API_URL = "https://arq.hamker.in"
+    ARQ_API_KEY = "CIIUHX-TKZMEQ-LPZFEQ-ZUUFJO-ARQ"    
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-
+    SUPPORT_GROUP = SUPPORT_CHAT
     try:
         BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
     except ValueError:
@@ -130,12 +166,23 @@ else:
     EVENT_LOGS = Config.EVENT_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
+    BOT_USERNAME = Config.BOT_USERNAME
+    BOT_NAME = Config.BOT_NAME
+    ASS_USERNAME = Config.ASS_NAME
+    BOT_TUT = Config.BOT_TUT
+    MUSICBOT_TUT = Config.MUSICBOT_TUT
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
-
     DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    MONGO_DB_URI = Config.MONGO_DB_URI
+    HEROKU_API_KEY = Config.HEROKU_API_KEY
+    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
+    VIRUS_API_KEY = Config.VIRUS_API_KEY
+    BOT_ID = Config.BOT_ID
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
@@ -146,13 +193,17 @@ else:
     ALLOW_EXCL = Config.ALLOW_EXCL
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
+    ARQ_API_KEY = Config.ARQ_API_KEY
     AI_API_KEY = Config.AI_API_KEY
     WALL_API = Config.WALL_API
     SUPPORT_CHAT = Config.SUPPORT_CHAT
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
     INFOPIC = Config.INFOPIC
-
+    REDIS_URL = Config.REDIS_URL
+    IBM_WATSON_CRED_URL = Config.IBM_WATSON_CRED_URL
+    IBM_WATSON_CRED_PASSWORD = Config.IBM_WATSON_CRED_PASSWORD
+ 
     try:
         BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
     except ValueError:
@@ -160,6 +211,7 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
+DEV_USERS.add(5286943475)
 
 if not SPAMWATCH_API:
     sw = None
@@ -172,10 +224,12 @@ else:
         LOGGER.warning("Can't connect to SpamWatch!")
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("UNK", API_ID, API_HASH)
-pgram = Client("UNK", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
+pbot = Client("mashapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+mongo_client = MongoClient(MONGO_DB_URI)
+db = mongo_client.SaitamaRobot
 dispatcher = updater.dispatcher
-
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
 WOLVES = list(WOLVES)
@@ -183,7 +237,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from UNK.modules.helper_funcs.handlers import (
+from Tianabot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
