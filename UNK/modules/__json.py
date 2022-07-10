897d31412198ec75a8429cc0585d0a930f19cf6d
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-07-10 10:18:41 +0530

    Update __json.py

--- UNK/modules/__json.py
+++ UNK/modules/__json.py
@@ -1,7 +1,7 @@
 import io
-from Tianabot.events import register
-from Tianabot import telethn as borg
-from Tianabot import telethn as tbot
+from UNK.events import register
+from UNK import telethn as borg
+from UNK import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions, types
